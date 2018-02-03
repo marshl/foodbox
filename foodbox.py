615bab9bd98fd94f5c3aa1c16f0b0225e858e884
marshl <marshl@live.com.au>
2018-02-03 20:01:19 +1100

    Added nltk support

--- foodbox.py
+++ foodbox.py
@@ -1,29 +1,51 @@
-import argparse, fileinput, re, random, sys
+import argparse, random, re, sys, warnings
+from nltk.corpus import wordnet as wn
 from PyDictionary import PyDictionary
 
+warnings.simplefilter('ignore')
 dictionary = PyDictionary()
+
+def get_pydictionary_synonyms(word, missing_letters):
+    synonyms = [x for x in dictionary.synonym(word) or [None] if x]
+    return reduce_synonyms(word, synonyms, missing_letters)
+
+
+def get_wordnet_synonyms(word, missing_letters):
+    if not wn.synsets(word):
+        return []
+    synonyms = [x for x in wn.synsets(word)[0].lemma_names()]
+    return reduce_synonyms(word, synonyms, missing_letters)
+
+
+def reduce_synonyms(word, synonyms, missing_letters):
+    # Remove words that are the same as the actual token
+    synonyms = [x for x in synonyms if x.lower() != word.lower()]
+    # Remove synonyms that have the forbidden letters
+    synonyms = [x for x in synonyms if not any(char in x for char in missing_letters)]
+    return synonyms
+
+
+def foodbox_translate_word(word, missing_letters):
+    if not any(char in word for char in missing_letters):
+        return word
+
+    synonyms = get_wordnet_synonyms(word, missing_letters) or get_pydictionary_synonyms(word, missing_letters)
+
+    if not synonyms:
+        return word
+
+    synonym = random.choice(synonyms)
+    if word[0].isupper():
+        synonym = synonym[0].upper() + synonym[1:]
+
+    return synonym
 
 
 def foodbox_translate(text, missing_letters):
     result = ''
 
     for token in re.split('(\w+)(?=\W)?', text):
-        if 'e' not in token:
-            result += token
-            continue
-
-        synonyms = [x for x in dictionary.synonym(token) or [None] if x]
-
-        synonyms = [word for word in synonyms if not any(char in word for char in missing_letters)]
-
-        if not synonyms:
-            result += token
-            continue
-
-        synonym = random.choice(synonyms)
-        if token[0].isupper():
-            synonym = synonym[0].upper() + synonym[1:]
-        result += synonym
+        result += foodbox_translate_word(token, missing_letters)
 
     return result
 
